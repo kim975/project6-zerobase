fe0d13e57e083fe380191e1048a167c6987a7fe5
kim <scilo200@gmail.com>
2024-09-08 04:46:55 +0900

    store token unique 설정

--- src/main/java/com/zerobase/project6/store/domain/model/Store.java
+++ src/main/java/com/zerobase/project6/store/domain/model/Store.java
@@ -17,6 +17,7 @@ public class Store extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(unique = true)
     private String storeToken;
 
     private Long storeOwnerId;
